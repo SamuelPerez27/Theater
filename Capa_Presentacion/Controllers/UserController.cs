c9b449c47fa7d63fafd4294009b4ef02c48e50f6
Samuelperez07 <samuepperez07@gmail.com>
2020-12-04 13:02:31 -0400

    Proyecto

---
+++ Capa_Presentacion/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Capa_Entidad;
+using Capa_Servicios;
+using Capa_Negocios;
+
+namespace Capa_Presentacion.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            ViewBag.Ultimas_Peliculas = Listar_Pelicula.ListarUltimaPelicula().ToList();
+            var pelicula = Listar_Pelicula.ListarPeliculas().ToList();
+            return View(pelicula);
+        }
+    }
+}
